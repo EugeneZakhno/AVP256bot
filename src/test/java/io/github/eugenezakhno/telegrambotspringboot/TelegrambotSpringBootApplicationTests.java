cf0517634c898eb00981aeb8c65b597ada916eea
EugeneZakhno <zohan1234567@gmail.com>
2021-03-29 22:19:54 +0300

    init

---
+++ src/test/java/io/github/eugenezakhno/telegrambotspringboot/TelegrambotSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eugenezakhno.telegrambotspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegrambotSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
