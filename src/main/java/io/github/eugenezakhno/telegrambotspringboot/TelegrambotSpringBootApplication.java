1159ad27081c1e21a157b02ef9aae6a24805295c
EugeneZakhno <zohan1234567@gmail.com>
2021-03-30 22:16:34 +0300

    codding

--- src/main/java/io/github/eugenezakhno/telegrambotspringboot/TelegrambotSpringBootApplication.java
+++ src/main/java/io/github/eugenezakhno/telegrambotspringboot/TelegrambotSpringBootApplication.java
@@ -2,11 +2,13 @@ package io.github.eugenezakhno.telegrambotspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class TelegrambotSpringBootApplication {
 
 	public static void main(String[] args) {
+		ApiContextInitializer.init();
 		SpringApplication.run(TelegrambotSpringBootApplication.class, args);
 	}
 
