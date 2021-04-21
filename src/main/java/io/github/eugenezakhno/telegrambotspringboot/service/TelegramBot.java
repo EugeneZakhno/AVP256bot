44f1906f63541703056d829664900656aca9e08b
EugeneZakhno <zohan1234567@gmail.com>
2021-04-21 21:37:03 +0300

    refactoring @OSONtestbot

--- src/main/java/io/github/eugenezakhno/telegrambotspringboot/service/TelegramBot.java
+++ src/main/java/io/github/eugenezakhno/telegrambotspringboot/service/TelegramBot.java
@@ -11,9 +11,7 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
 
 
 @Component
