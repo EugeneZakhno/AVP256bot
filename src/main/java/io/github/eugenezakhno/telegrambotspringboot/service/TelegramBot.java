6675ea513b8ddf79cdafdf125e2497f5a17aff2e
EugeneZakhno <zohan1234567@gmail.com>
2021-04-29 22:34:08 +0300

    refactoring @OSONtestbot

--- src/main/java/io/github/eugenezakhno/telegrambotspringboot/service/TelegramBot.java
+++ src/main/java/io/github/eugenezakhno/telegrambotspringboot/service/TelegramBot.java
@@ -3,16 +3,13 @@ package io.github.eugenezakhno.telegrambotspringboot.service;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
-import lombok.extern.log4j.Log4j2;
+
 
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-
-
 @Component
-@Log4j2
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class TelegramBot extends TelegramLongPollingBot {
 
